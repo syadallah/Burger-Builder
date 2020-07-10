95126fb04f48128793b95e2e97bfea702c4fab02
syadallah <syadallah@gmail.com>
2020-07-10 16:09:28 -0400

    Solve some source issues

--- src/components/Burger/Burgeringredient/Burgeringredient.js
+++ src/components/Burger/Burgeringredient/Burgeringredient.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react'
 import PropTypes from 'prop-types'
-import classes from './burgeringredient/burgeringredient.css'
+import classes from './Burgeringredient.css'
 
 
 class BurgerIngredient extends Component {
