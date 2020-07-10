4f66ecaa95beb534ca6123041959a867f28613c0
syadallah <syadallah@gmail.com>
2020-07-10 15:57:52 -0400

    Add Burger Component
    Burger component added to include BurgerIngredient component
    Style added to the Burger component
    @media style added

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Burger/css'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
+const burger = (props) => {
+  return (
+    <div className={classes.Burger}>
+         <BurgerIngredient type="" />
+    </div>
+  )
+}
+export default burger
