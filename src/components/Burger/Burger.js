7875766143ae8808b7f7f7dd313a2a0d433151c4
syadallah <syadallah@gmail.com>
2020-07-21 19:57:27 -0400

    Debug in Burgerjs

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,9 +6,10 @@ const burger = (props) => {
   const transformedIngredients = Object.key(props.ingredients)
       .map(igKey => {
         return [...Array(props.ingredients[igKey])].map(_, i) => {
-          <BurgerIngredient key={igKey + i} type={igKey} />
+          <BurgerIngredient key= { igKey i} type={igKey} />
         }
       })
+
   return (
     <div className={classes.Burger}>
          <BurgerIngredient type="bread-top" />
