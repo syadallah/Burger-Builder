1aafa4ef741bf3d0fc1eb54c0746d70f4c62e124
syadallah <syadallah@gmail.com>
2020-07-23 21:51:56 -0400

    Remove the beard top from Burger.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -12,7 +12,7 @@ const burger = (props) => {
 
   return (
     <div className={classes.Burger}>
-         <BurgerIngredient type="bread-top" />
+        
          <BurgerIngredient type="cheese" />
          <BurgerIngredient type="meat" />
          <BurgerIngredient type="bread-bottom" />
