866351a7f50d0abcf53ac1962296948f5ca8fa29
syadallah <syadallah@gmail.com>
2020-07-24 21:58:18 -0400

    Use React.Fragment instead of <Aux>
    <Aux> is hoc
    see hoc foder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 
-import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
   render () {
     return (
-      <Aux>
+      <React.Fragment>
          <Burger ingredients= {this.state.ingredients}/>
          <div>Build Control</div>
-      </Aux>
+      </React.Fragment>
     )
   }
 }
