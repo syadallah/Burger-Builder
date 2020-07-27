b25b1227b4a09d353a55ecde84901fe8991b438f
syadallah <syadallah@gmail.com>
2020-07-26 23:14:03 -0400

    Fix the arrow function error

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,14 +5,15 @@ import BurgerIngredient from './Burgeringredient/Burgeringredient.js'
 const burger = (props) => {
   const transformedIngredients = Object.key(props.ingredients)
       .map(igKey => {
-        return [...Array(props.ingredients[igKey])].map(_, i) => {
+        return [...Array(props.ingredients[igKey])].map(_,i) =>
+        {
           <BurgerIngredient key= { igKey i} type={igKey} />
         }
       })
 
   return (
     <div className={classes.Burger}>
-        
+
          <BurgerIngredient type="cheese" />
          <BurgerIngredient type="meat" />
          <BurgerIngredient type="bread-bottom" />
