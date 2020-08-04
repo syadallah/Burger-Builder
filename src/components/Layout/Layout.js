6245344c16298467570eab8ad3e1344e1e0da5f8
syadallah <syadallah@gmail.com>
2020-08-04 18:44:09 -0400

    Replace <Aux> with <React.Fragment>

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import Aux from '../../hoc/Aux'
 import classes from './Layout.css'
 
 const layout = ( props ) => (
-  <Aux>
+  <React.Fragment>
   <div>Toolbar, SideDrawer, Backdrop</div>
   <main className={classes.Content}>
   {props.children}
   </main>
-  </Aux>
+  </React.Fragment>
 )
 
 export default layout
