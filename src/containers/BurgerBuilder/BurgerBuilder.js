a46a6fde635cae034be4d53e3c5ee80329cf80c7
syadallah <syadallah@gmail.com>
2020-09-02 18:32:37 -0400

    Pass the totalPrice as a props from BurgerBuilder to BuildControls
    toFixed used to show only 2 decimal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -67,6 +67,8 @@ class BurgerBuilder extends Component {
          ingredientAdded={this.addIngredientHandler}
          ingredientRemoved={this.removeIngredientHandler}
          disabled ={disabledInfo}
+         price={this.state.totalPrice}
+
 
 
          />
