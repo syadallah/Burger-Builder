0fde2c1a516763984969f2a57b1b59ec04954f4e
syadallah <syadallah@gmail.com>
2020-09-02 17:16:55 -0400

    Add remove Ingredient listner to BurgerControl
    Event listner was handled by the removeIngredientHandler method
    in the BurgerBuilder Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -44,7 +44,7 @@ class BurgerBuilder extends Component {
     updatedIngredients[type] = updatedCount
     const priceDeduction = INGREDIENT_PRICES[type]
     const oldPrice = this.state.totalPrice
-    const newPrice = oldPrice + priceDeduction
+    const newPrice = oldPrice - priceDeduction
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
 
 
@@ -55,6 +55,8 @@ class BurgerBuilder extends Component {
          <Burger ingredients= {this.state.ingredients}/>
          <BuildControls
          ingredientAdded={this.addIngredientHandler}
+         ingredientRemoved={this.removeIngredientHandler}
+
 
          />
       </React.Fragment>
