ce1baeadc7eb388b7c090adf58458fd6affd0696
syadallah <syadallah@gmail.com>
2020-09-28 17:26:17 -0400

    Fix the way Logo Component is imported to Toolbar

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import burgerLogo from '../../assets/images/burger-logo.png'
-import style from './Logo.css'
+import style from './logo.css'
 const logo = (props) => (
   <div className={style.Logo}>
   <img src={burgerLogo} alt="My logo" />
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import style from './Toolbar.css'
-import burgerLogo from '../../../Logo/Logo'
+import Logo from '../../Logo/Logo'
 const toolbar = (props) => (
   <header className={style.Toolbar}>
   <div>MENU</div>
-  <div>{burgerLogo}</div>
+  <Logo />
   <nav>
   ...
   </nav>
