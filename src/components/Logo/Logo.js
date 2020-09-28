35ffc896163d6016d06de80df256ff8a73d1d5b4
syadallah <syadallah@gmail.com>
2020-09-28 17:23:48 -0400

    Import style to Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import burgerLogo from '../../assets/images/burger-logo.png'
+import style from './Logo.css'
 const logo = (props) => (
-  <div>
+  <div className={style.Logo}>
   <img src={burgerLogo} alt="My logo" />
   </div>
 
