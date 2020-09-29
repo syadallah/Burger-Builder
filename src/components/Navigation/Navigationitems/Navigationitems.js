7decb0cd6dbcf6dfde47523e3f1f93538b663ecd
syadallah <syadallah@gmail.com>
2020-09-29 14:52:37 -0400

    Import NavigationItem Component to NavigationItems

--- src/components/Navigation/Navigationitems/Navigationitems.js
+++ src/components/Navigation/Navigationitems/Navigationitems.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import style from './NavigationItems.css'
+import NavigationItem from './NavigationItem/NavigationItem'
 const navigationItems = () => (
   <ul className={style.NavigationItems}>
+  <NavigationItem />
 
   </ul>
 
