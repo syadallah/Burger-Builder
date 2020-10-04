ef3ad077ed4f406590f8eb9ab9d8788ef3dbc7c2
syadallah <syadallah@gmail.com>
2020-10-04 17:36:34 -0400

    Import Backdrop into SideDrawer

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import burgerLogo from '../../assets/images/burger-logo.png'
 import style from './logo.css'
 const logo = (props) => (
-  <div className={style.Logo}>
+  <div className={style.Logo} style={{height: props.height}}>
   <img src={burgerLogo} alt="My logo" />
   </div>
 
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,20 @@ import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import style from './SideDrawer.css'
-
+import Backdrop from '../../UI/Backdrop/Backdrop'
 const sideDrawer = (props) => {
   //..
 
   return (
+    <div>
+    <Backdrop />
 
     <div className={style.SideDrawer}>
-    <Logo />
+    <Logo height='11%'/>
     <nav>
     <NavigationItems />
     </nav>
+    </div>
 
     </div>
   )
