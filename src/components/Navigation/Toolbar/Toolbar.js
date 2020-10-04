7ef20e0b2b2e1fea894cc74cb3ec0c546aaecfb5
syadallah <syadallah@gmail.com>
2020-10-04 19:19:19 -0400

    Import the drawerToggle component to Toolbar
    DrawerToggle component replaced the Menu <div> in Toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,9 +2,11 @@ import React from 'react'
 import style from './Toolbar.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
+
 const toolbar = (props) => (
   <header className={style.Toolbar}>
-  <div>MENU</div>
+  <DrawerToggle />
   <Logo />
   <nav className={style.DesktopOnly}>
   <NavigationItems />
