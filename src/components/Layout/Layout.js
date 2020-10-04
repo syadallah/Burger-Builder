8bf0caf73345a9d279c5a814916cefd734aeddae
syadallah <syadallah@gmail.com>
2020-10-04 19:10:57 -0400

    Reusing Bakdrop component on the SideDrawer
    Backdrop used to hide the SideDrawer on the mobile devices
    less than 500px

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,24 @@
-import React, {component} from 'react'
+import React, {Component} from 'react'
 import styles from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
-class Layout extends component {
+
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+  sideDrawerCloseHandler = () => {
+    this.setState({showSideDrawer: false})
+
+  }
   render () {
     return (
       <React.Fragment>
       <Toolbar />
-      <SideDrawer />
+      <SideDrawer
+
+      closed={this.sideDrawerCloseHandler}
+      open={this.state.showSideDrawer}/>
       <main className={styles.Content}>
       {this.props.children}
       </main>
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,14 +3,20 @@ import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import style from './SideDrawer.css'
 import Backdrop from '../../UI/Backdrop/Backdrop'
+
 const sideDrawer = (props) => {
-  //..
+  let attachedClasses = [style.SideDrawer, style.Close]
+  if (props.open) {
+    attachedClasses = [style.SideDrawer, style.Open]
+  }
+
 
   return (
     <div>
-    <Backdrop show/>
+    <Backdrop show={props.open} clicked={props.closed}/>
 
-    <div className={style.SideDrawer}>
+
+    <div className={attachedClasses.join(' ')}>
     <Logo height='10%' margin-bottom="30px"/>
     <nav>
     <NavigationItems />
