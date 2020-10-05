9645e083470c32b86003e0bcaf00f23a21b7318a
syadallah <syadallah@gmail.com>
2020-10-05 18:40:44 -0400

    Set "loading" to false after receiving a response from the server
    When loading is false, the Spinner will be hide

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -100,8 +100,12 @@ class BurgerBuilder extends Component {
       deliveryMethod: 'Fast'
     }
       axios.post('/orders.json', order)
-      .then(response => console.log(response))
-      .catch(error =>console.log(error))
+      .then(response => {
+        this.setState({ loading: false})
+      })
+      .catch(error => {
+        this.setState({ loading: false})
+      })
 
     }
   render () {
