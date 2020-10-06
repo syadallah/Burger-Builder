45dce8c9c6992b41a8f6a97ac299b021eb13e62f
syadallah <syadallah@gmail.com>
2020-10-06 19:52:46 -0400

    Fix the Button component
    a space was added to join the className of Button and btnType

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './Button.css'
 const button = (props) => (
   <button
-  className={[styles.Button, styles[props.btnType]].join('')}
+  className={[styles.Button, styles[props.btnType]].join(' ')}
   onClick={props.clicked}></button>
 
 )
