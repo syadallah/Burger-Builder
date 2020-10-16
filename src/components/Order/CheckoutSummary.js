c25a2316f39db0c866c309e56628dcc2fe145248
syadallah <syadallah@gmail.com>
2020-10-16 18:12:54 -0400

    Add style to checkoutSummary Component

--- src/components/Order/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Burger from '../../Burger/Burger'
 import Button from './UI/Button/Button'
-
+import style from './CheckoutSummary.css'
 const checkoutSummary = (pros) => {
   return (
-<div>
+<div clasName={style.CheckoutSummary}>
 <h1> We hope it tastes good!</h1>
 <div style ={{width: '300px', height: '300px', margin: 'auto'}}>
 <Burger />
 </div>
-<Button btnType="Danger">CANCEL</Button>
-<Button btnType="Success">CONTINUE</Button>
+<Button btnType="Danger"
+clicked>CANCEL</Button>
+<Button btnType="Success"
+clicked>CONTINUE</Button>
 
 
 </div>
