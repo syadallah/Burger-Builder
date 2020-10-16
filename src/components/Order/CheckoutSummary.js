274c8b6370fc824457b6a15af685ced82c53fa71
syadallah <syadallah@gmail.com>
2020-10-16 18:25:18 -0400

    Pass the props from CheckoutSummary to Checkout
    The purpose from the Checkout Component is to show the client a summary
    of the order when click on the checkout button on the toolbar

--- src/components/Order/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Burger from '../../Burger/Burger'
 import Button from './UI/Button/Button'
 import style from './CheckoutSummary.css'
-const checkoutSummary = (pros) => {
+const checkoutSummary = (props) => {
   return (
 <div clasName={style.CheckoutSummary}>
 <h1> We hope it tastes good!</h1>
 <div style ={{width: '300px', height: '300px', margin: 'auto'}}>
-<Burger />
+<Burger ingredients={props.ingredients}/>
 </div>
 <Button btnType="Danger"
 clicked>CANCEL</Button>
