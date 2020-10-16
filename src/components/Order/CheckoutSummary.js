c4022c234aa1731e8d3faf872968feb598838727
syadallah <syadallah@gmail.com>
2020-10-16 18:03:02 -0400

    Start building the checkoutSummary function Component

---
+++ src/components/Order/CheckoutSummary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const checkoutSummary = (pros) => {
+  return (
+<div>
+<h1> We hope it tastes good!</h1>
+<div style ={{width: '300px', height: '300px', margin: 'auto'}}>
+</div>
+</div>
+)
+}
+
+export default checkoutSummary
