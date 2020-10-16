a57ad532105a7cb65dca9bda6def9e2a400f5615
syadallah <syadallah@gmail.com>
2020-10-16 18:08:52 -0400

    Import Button Component to the checkoutSummary

--- src/components/Order/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Burger from '../../Burger/Burger'
+import Button from './UI/Button/Button'
 
 const checkoutSummary = (pros) => {
   return (
@@ -8,6 +9,10 @@ const checkoutSummary = (pros) => {
 <div style ={{width: '300px', height: '300px', margin: 'auto'}}>
 <Burger />
 </div>
+<Button btnType="Danger">CANCEL</Button>
+<Button btnType="Success">CONTINUE</Button>
+
+
 </div>
 )
 }
