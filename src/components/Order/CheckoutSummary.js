b37ea4ef33d1d73a4cbbadd002e72f28c1f749a6
syadallah <syadallah@gmail.com>
2020-10-16 18:32:04 -0400

    Make the test attaached to Checkout align in center

--- src/components/Order/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Burger from '../../Burger/Burger'
-import Button from './UI/Button/Button'
+import Burger from '../Burger/Burger'
+import Button from '../UI/Button/Button'
 import style from './CheckoutSummary.css'
 const checkoutSummary = (props) => {
   return (
