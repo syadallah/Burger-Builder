0ef4a9272d5b0c9a62ab797c4afa00310cbf622b
syadallah <syadallah@gmail.com>
2020-10-16 18:38:54 -0400

    Use Media Queries to create a mobile version of the Checkout Component

--- src/components/Order/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary.js
@@ -6,7 +6,7 @@ const checkoutSummary = (props) => {
   return (
 <div clasName={style.CheckoutSummary}>
 <h1> We hope it tastes good!</h1>
-<div style ={{width: '300px', height: '300px', margin: 'auto'}}>
+<div style ={{width: '100%', margin: 'auto'}}>
 <Burger ingredients={props.ingredients}/>
 </div>
 <Button btnType="Danger"
