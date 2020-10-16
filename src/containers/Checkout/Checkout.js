d7d938b903ae6147cc33b4cb749254f999a460e9
syadallah <syadallah@gmail.com>
2020-10-16 18:16:01 -0400

    Import CheckoutSummary Component to Checkout

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,7 +1,14 @@
 import React, { Component} from 'react'
+import CheckoutSummary from '../../components/Order/CheckoutSummary'
 
 class Checkout extends Component {
-
+render () {
+  return (
+    <div>
+    <CheckoutSummary />
+    </div>
+  )
+}
 }
 
-export default  Checkout 
+export default  Checkout
