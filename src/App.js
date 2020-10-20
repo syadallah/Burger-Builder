794eaf80784205a23832d86394c4938b3ddc7232
syadallah <syadallah@gmail.com>
2020-10-20 18:11:28 -0400

    Use <Switch>
    in the App.js use <Swith> so we only load one of the routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
@@ -8,9 +8,10 @@ function App() {
   return (
     <div>
     <Layout />
-    <Route path="/" component={BurgerBuilder}/>
-    <Route path="/Checkout" component={Checkout}/>
-
+    <Switch>
+    <Route path="/" excat component={BurgerBuilder}/>
+    <Route path="/checkout" component={Checkout}/>
+    </Switch>
     </div>
   );
 }
