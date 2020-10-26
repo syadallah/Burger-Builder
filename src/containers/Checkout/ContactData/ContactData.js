368db70ad2db2cb7692b49e8548a0439e2a29e29
syadallah <syadallah@gmail.com>
2020-10-25 20:14:49 -0400

    Add form to inter the contact data inside ContactData component
    inputs
    Name
    Email
    Street
    Postal Code

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -9,4 +9,17 @@ class ContactData extends Component {
       postalCode:''
     }
   }
+  render () {
+    return (
+      <div>
+      <h4> Enter your contact data</h4>
+      <form>
+      <input type='text' name='name' placeholder='Your Name' />
+      <input type='text' name='email' placeholder='Your Mail' />
+      <input type='text' name='street' placeholder='Street' />
+      <input type='text' name='name' placeholder='Postal Code' />
+      </form>
+      </div>
+    )
+  }
 }
