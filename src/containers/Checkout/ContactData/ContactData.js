a1eed76415a40738e98fab793ca22c2f242f380d
syadallah <syadallah@gmail.com>
2020-10-25 20:09:08 -0400

    Fetch contact date and set state to manage it

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class ContactData extends Component {
+  state ={
+    name: '',
+    email: '',
+    address: {
+      street: '',
+      postalCode:''
+    }
+  }
+}
