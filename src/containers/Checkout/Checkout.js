d5ce75f1452fdc7f5e15fb14e89face5daf91e87
syadallah <syadallah@gmail.com>
2020-10-30 20:40:27 -0400

    In the Checkout component handle the price

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,20 +4,22 @@ import { Route } from 'react-router-dom'
 import ContactData from './ContactData/ContactData'
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad:1,
-      meat:1,
-      cheese:1,
-      bacon:1
-    }
+    ingredients: null,
+    price:0
   }
   componentDidMount() {
     const query = new URLSearchParams(this.props.location.search)
     const ingredients = {}
+    let price = 0
     for (let param of query.entries()) {
+      if (param[0] === 'price') {
+        price = param[1]
+      } else {
+        ingredients[param[0]] = +param[1]
+      }
       ingredients[param[0]] = +param[1]
     }
-    this.setState({ingredients:ingredients})
+    this.setState({ingredients:ingredients, totalPrice: price})
   }
   checkoutCancelledHandler = () => {
     this.props.history.goBack()
