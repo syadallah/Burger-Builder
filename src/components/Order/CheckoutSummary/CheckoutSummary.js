11df5c70d754908e110b4f352e6598c81b16db28
syadallah <syadallah@gmail.com>
2020-11-03 20:35:13 -0500

    Remove the Burger component from CheckoutSummary

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,23 +1,22 @@
 import React from 'react'
-import Burger from '../../Burger/Burger'
 import Button from '../../UI/Button/Button'
 import style from './CheckoutSummary.css'
 
-const checkoutSummary = (props) => {
-  return (
+const checkoutSummary = (props) =>
 <div className={style.CheckoutSummary}>
 <h1> We hope it tastes good!</h1>
 <div style ={{width: '100%', margin: 'auto'}}>
-<Burger ingredients={props.ingredients}/>
 </div>
-<Button btnType="Danger"
+
+<Button
+btnType="Danger"
 clicked={props.checkoutCancelled} text={'CANCEL'}></Button>
-<Button btnType="Success"
+<Button
+btnType="Success"
 clicked={props.checkoutContinued} text={'Checkout'}></Button>
 
 
 </div>
-)
-}
+
 
 export default checkoutSummary
