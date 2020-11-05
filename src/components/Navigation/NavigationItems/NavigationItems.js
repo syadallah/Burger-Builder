b8d9b78330c0bd65f053daca509069291705dd5a
syadallah <syadallah@gmail.com>
2020-11-05 18:42:44 -0500

    Add Orders to navigationItems component
    /orders will go to Your Orders page

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import NavigationItem from './NavigationItem/NavigationItem'
 const navigationItems = () => (
   <ul className={style.NavigationItems}>
   <NavigationItem link='/' active>Burger Builder</NavigationItem>
-  <NavigationItem link='/'>Checkout</NavigationItem>
+  <NavigationItem link='/orders'>Your Orders</NavigationItem>
 
   </ul>
 
