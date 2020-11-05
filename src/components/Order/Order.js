e80e7cec8ab17e48f7f65c77cd81d7956593f464
syadallah <syadallah@gmail.com>
2020-11-05 18:09:10 -0500

    Add style to Order component

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import style from './Order.css'
 
 const order = (props) => (
-  <div>
+  <div className={style.Order}>
   <p> Ingredients: Salad(1)</p>
   <p>Price: <strong>USD 5.45</strong></p>
   </div>
