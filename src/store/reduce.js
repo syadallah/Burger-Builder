8d764e16cefb2b2ba768c98a62853a55109d540d
syadallah <syadallah@gmail.com>
2020-11-09 14:52:25 -0500

    Finishing the Reducer for Ingredients

--- src/store/reduce.js
+++ src/store/reduce.js
@@ -11,7 +11,27 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case actionTypes.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]:state.ingredients[action.ingredientName] + 1
+        }
+      }
 
+      case actionTypes.REMOVE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]:state.ingredients[action.ingredientName] - 1
+
+      }
+    }
+    default: return state
+}
 }
 
 export default reducer
